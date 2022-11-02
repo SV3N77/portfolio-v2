bcf1244572d494622fef6803d0ea4336e1f5fb5c
SV3N77 <danny.jiang1234@hotmail.com>
2022-11-02 13:38:47 +1100

    add Layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Header from "components/Header";
-import Footer from "components/Footer";
+import Layout from "components/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="flex min-h-screen flex-col">
-      <Header />
-      <div className="grow">
-        <Component {...pageProps} />
-      </div>
-
-      <Footer />
-    </div>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
